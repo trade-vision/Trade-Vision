5d1174f226bcd330d1a9332d7d9f948dbebefd6e
Akin <akinpds@gmail.com>
2019-06-24 14:06:45 -0500

    Curriculum button goes to curriculum widget

---
+++ lib/curriculum/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+   build(context) {
+     return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Curriculum'),
+          backgroundColor: new Color(0xFF151026),
+            ), 
+            // body: 
+          ),
+        );
+   }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tradevision/services.dart';
-
+import 'package:tradevision/curriculum/home.dart';
 
 
 void main() => runApp(MyApp());
@@ -9,6 +9,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   build(context){
     return MaterialApp(
+      initialRoute: '/',
+      routes: {
+    // When navigating to the "/second" route, build the SecondScreen widget.
+    '/curriculum': (context) => HomePage()},
       home: Scaffold(
         appBar: AppBar(
           title: Text('Trade Vision'),
