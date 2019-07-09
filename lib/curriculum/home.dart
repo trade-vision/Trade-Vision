af833a6bb03c9b817bd92d90a6bbf0eb5310b035
asp2131 <akinpds@gmail.com>
2019-07-09 10:16:12 -0500

    Changed homepage UI to grid layout for orientation

--- lib/curriculum/home.dart
+++ lib/curriculum/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sparkline/flutter_sparkline.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 
@@ -11,15 +10,8 @@ class HomePage extends StatefulWidget
 
 class _HomePageState extends State<HomePage>
 {
-  final List<List<double>> charts =
-  [
-    [0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4],
-    [0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4, 0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4,],
-    [0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4, 0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4, 0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4]
-  ];
-
-  static final List<String> chartDropdownItems = [ 'Last 7 days', 'Last month', 'Last year' ];
-  String actualDropdown = chartDropdownItems[0];
+ 
+  
   int actualChart = 0;
 
   @override
@@ -146,12 +138,39 @@ class _HomePageState extends State<HomePage>
               ),
             ),
           ),
+          _buildTile(
+            Padding
+            (
+              padding: const EdgeInsets.all(24.0),
+              child: Column
+              (
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>
+                [
+                  Material
+                  (
+                    color: Colors.amber,
+                    shape: CircleBorder(),
+                    child: Padding
+                    (
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(Icons.notifications, color: Colors.white, size: 30.0),
+                    )
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                  Text('CSS', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 24.0)),
+                  Text('Style', style: TextStyle(color: Colors.black45)),
+                ]
+              ),
+            ),
+          ),
         ],
         staggeredTiles: [
-          StaggeredTile.extent(2, 110.0),
-          StaggeredTile.extent(1, 180.0),
-          StaggeredTile.extent(1, 180.0),
-          StaggeredTile.extent(2, 220.0),
+          StaggeredTile.extent(4, 110.0),
+          StaggeredTile.extent(4, 180.0),
+          StaggeredTile.extent(4, 180.0),
+          StaggeredTile.extent(4, 220.0),
           StaggeredTile.extent(2, 110.0),
         ],
       )
--- lib/services.dart
+++ lib/services.dart
@@ -1,79 +1,171 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
 
 class Services extends StatelessWidget {
 
   @override
    build(context) {
-    return Column(children: [
-            Card(child: Column(children: <Widget>[
-              Image.asset('assets/newLogo.png')
-            ], 
-            ), 
-            ), 
-            Column(children: <Widget>[
-            ButtonTheme(
-              minWidth: 300.0,
-              height: 100.0,
-              buttonColor: Colors.green[200],
-              child: RaisedButton(
-                child: Text(
-                  'Curriculum',
-                   style: new TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black,
+    return StaggeredGridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        children: <Widget>[
+          _buildTile(
+            Padding
+            (
+              padding: const EdgeInsets.all(24.0),
+              child: Row
+              (
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>
+                [
+                  Column
+                  (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>
+                    [
+                      Text('Trade Vision', style: TextStyle(color: Colors.blueAccent, fontSize: 34.0)),
+                      Text('Mobile Web Development Bootcamp', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 16.0))
+                    ],
                   ),
-                ),
-                onPressed: ()=>{
-                   Navigator.pushNamed(context, '/curriculum')
-                },
+                  
+                ]
               ),
-            ), 
-            ButtonTheme(
-              minWidth: 300.0,
-              height: 100.0,
-              buttonColor: Colors.blue,
-              child: RaisedButton(
-                child: Text(
-                  'Games',
-                   style: new TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black,
+            ),
+          ),
+          _buildTile(
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column
+              (
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>
+                [
+                  Material
+                  (
+                    color: Colors.teal,
+                    shape: CircleBorder(),
+                    child: Padding
+                    (
+                      padding: const EdgeInsets.all(16.0),
+                      child: Icon(Icons.settings_applications, color: Colors.white, size: 30.0),
+                    )
                   ),
-                ),
-                onPressed: ()=>{},
+                  Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                  Text('Learn', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 24.0)),
+                  Text('Curriculum/Lessons', style: TextStyle(color: Colors.black45)),
+                ]
               ),
-            ),  ]),
-            Column(children: <Widget>[
-            ButtonTheme(
-              minWidth: 300.0,
-              height: 100.0,
-              buttonColor: Colors.purple[200],
-              child: RaisedButton(
-                child: Text(
-                  'About Us',
-                   style: new TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black,
+            ),
+          ),
+          _buildTile(
+            Padding
+            (
+              padding: const EdgeInsets.all(24.0),
+              child: Column
+              (
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>
+                [
+                  Material
+                  (
+                    color: Colors.pink,
+                    shape: CircleBorder(),
+                    child: Padding
+                    (
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(IconData(57377, fontFamily: 'MaterialIcons'), color: Colors.white, size: 30.0),
+                    )
                   ),
-                ),
-                onPressed: ()=>{},
+                  Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                  Text('Games', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 24.0)),
+                  Text('All ages+', style: TextStyle(color: Colors.black45)),
+                ]
               ),
-            ), 
-            ButtonTheme(
-              minWidth: 300.0,
-              height: 100.0,
-              buttonColor: Colors.pink[200],
-              child: RaisedButton(
-                child: Text(
-                  'Be Apart of the Vision',
-                   style: new TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black,
+            ),
+          ),
+           _buildTile(
+            Padding
+            (
+              padding: const EdgeInsets.all(24.0),
+              child: Column
+              (
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>
+                [
+                  Material
+                  (
+                    color: Colors.amber,
+                    shape: CircleBorder(),
+                    child: Padding
+                    (
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(IconData(59387, fontFamily: 'MaterialIcons')),
+                    )
                   ),
-                ),
-                onPressed: ()=>{},
+                  Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                  Text('Join Us', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 24.0)),
+                  Text('Be Apart of the Vision', style: TextStyle(color: Colors.black45, fontSize: 12.0)),
+                ]
               ),
-            ),  ]),
-            ]);
+            ),
+          ),
+           _buildTile(
+            Padding
+            (
+              padding: const EdgeInsets.all(24.0),
+              child: Column
+              (
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>
+                [
+                  Material
+                  (
+                    color: Colors.cyan[300],
+                    shape: CircleBorder(),
+                    child: Padding
+                    (
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(Icons.notifications, color: Colors.white, size: 30.0),
+                    )
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 16.0)),
+                  Text('About Us', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 24.0)),
+                  Text('Our Story', style: TextStyle(color: Colors.black45)),
+                ]
+              ),
+            ),
+          ),
+        ],
+        staggeredTiles: [
+          StaggeredTile.extent(2, 200.0),
+          StaggeredTile.extent(1, 180.0),
+          StaggeredTile.extent(1, 180.0),
+          StaggeredTile.extent(1, 180.0),
+          StaggeredTile.extent(1, 180.0),
+        ],
+      );
   }
 }
+
+Widget _buildTile(Widget child, {Function() onTap}) {
+    return Material(
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(12.0),
+      shadowColor: Color(0x802196F3),
+      child: InkWell
+      (
+        // Do onTap() if it isn't null, otherwise do print()
+        onTap: onTap != null ? () => onTap() : () { print('Not set yet'); },
+        child: child
+      )
+    );
+  }
